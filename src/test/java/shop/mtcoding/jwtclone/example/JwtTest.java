3006417ae0034f828fb71eb2d0d331e8c1c921d7
Tokitatka <artigen3@gmail.com>
2023-03-20 19:16:26 +0900

    verification of jwt token created with test mode

--- src/test/java/shop/mtcoding/jwtclone/example/JwtTest.java
+++ src/test/java/shop/mtcoding/jwtclone/example/JwtTest.java
@@ -1,5 +1,45 @@
 package shop.mtcoding.jwtclone.example;
 
-public class JwtTest {
+import java.util.Date;
 
+import org.junit.jupiter.api.Test;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+
+public class JwtTest {
+    @Test
+    public void createJwt_test() {
+        // given
+        // when
+        String jwt = JWT.create()
+                .withSubject("myJwtToken")// token name
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)) // 7days after
+                .withClaim("id", 1) // detail
+                .withClaim("role", "guest")
+                .sign(Algorithm.HMAC512("secretKey")); // electronic signature
+        // then
+    }
+
+    @Test
+    public void verifyJwt_test() {
+        // given
+        String jwt = JWT.create()
+                .withSubject("myJwtToken")// token name
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60 * 24 * 7)) // 7days after
+                .withClaim("id", 1) // detail
+                .withClaim("role", "guest")
+                .sign(Algorithm.HMAC512("secretKey")); // electronic signature
+        // when
+        // 1. create mock jwt token -> Decode
+        DecodedJWT decodedJWT = JWT.require(Algorithm.HMAC512("secret")).build().verify(jwt);
+        // 2. Id verification
+        int id = decodedJWT.getClaim("id").asInt();
+        System.out.println(id);
+        // 3. Role verification
+        String role = decodedJWT.getClaim("role").asString();
+        System.out.println(role);
+        // then
+    }
 }
